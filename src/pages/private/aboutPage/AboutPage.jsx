13e96dd4befff8f94e942e05a09c1a4893973ca2
Mominul072 <mominul.info.cse@gmail.com>
2024-10-07 11:25:00 +0600

    modified about page

--- src/pages/private/aboutPage/AboutPage.jsx
+++ src/pages/private/aboutPage/AboutPage.jsx
@@ -1,28 +1,128 @@
 const AboutPage = () => {
   return (
-    <section className="grid lg:grid-cols-2 grid-cols-1 items-center   mx-auto w-[90%] gap-10 ">
-      {/* ========= writing section ========= */}
-      <div className="">
-        <h3 className="text-3xl font-semibold text-blue-600 font-serif">
-          About us
-        </h3>
-        <p className=" text-justify mt-6  ">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
-          adipisci cupiditate exercitationem debitis sit voluptates autem esse?
-          Nesciunt repellendus, tenetur exercitationem autem sint reiciendis
-          porro nihil temporibus animi provident? Enim similique odit cumque
+    // <section className="grid lg:grid-cols-2 grid-cols-1 items-center   mx-auto w-[90%] gap-10 ">
+    //   {/* ========= writing section ========= */}
+    //   <div className="">
+    //     <h3 className="text-3xl font-semibold text-blue-600 font-serif">
+    //       About us
+    //     </h3>
+    //     <p className=" text-justify mt-6  ">
+    //       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+    //       adipisci cupiditate exercitationem debitis sit voluptates autem esse?
+    //       Nesciunt repellendus, tenetur exercitationem autem sint reiciendis
+    //       porro nihil temporibus animi provident? Enim similique odit cumque
+    //     </p>
+
+    //     <div className="mt-8">
+    //       <button className="btn btn-xs bg-blue-400 rounded-2xl">
+    //         LEARN MORE
+    //       </button>
+    //     </div>
+    //   </div>
+
+    //   {/*========= picture section ========= */}
+    //   <div className="w-full">
+    //     <img src="/src/assets/image/Ecommerce web page (1).gif" alt="" />
+    //   </div>
+    // </section>
+
+    // <div className="hero bg-base-200 ">
+    //   <div className="hero-content text-center">
+    //     <div className="">
+    //       <h1 className="text-5xl font-bold ">About Us</h1>
+    //       <p className="py-6 justify-center">
+    //         Eonbazar Quality Leader Online Shop in Bangladesh. Eonbazar Sister
+    //         Concerns of Eon Group of Industries. Eon group is a fast-growing
+    //         conglomerate in Agri-based industry working to provide inputs to
+    //         farmers (crop & animal) to produce high yield and high-quality farm
+    //         which is produced for consumers. This group is engaged in
+    //         manufacturing, import and distributing all types of agricultural
+    //         inputs primarily for the nation-wide local market and also for the
+    //         global market. Our group is working with the vision to be a
+    //         contributor to “Food Security and Safety” in Bangladesh.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <section className="">
+      <div className="text-center mt-10">
+        <h3 className="text-3xl font-bold ">About Us</h3>
+      </div>
+      <div className="p-6">
+        <p className="mb-2 text-justify">
+          Eonbazar Quality Leader Online Shop in Bangladesh. Eonbazar Sister
+          Concerns of Eon Group of Industries. Eon group is a fast-growing
+          conglomerate in Agri-based industry working to provide inputs to
+          farmers (crop & animal) to produce high yield and high-quality farm
+          which is produced for consumers. This group is engaged in
+          manufacturing, import and distributing all types of agricultural
+          inputs primarily for the nation-wide local market and also for the
+          global market. Our group is working with the vision to be a
+          contributor to “Food Security and Safety” in Bangladesh.
+        </p>
+        <p className="mb-4">Inception: January 2000</p>
+        <p>Number of Employees: approx. 1400</p>
+        <p className="mb-4">
+          <span className="font-bold">TIN Number:</span> 596870980285
         </p>
 
-        <div className="mt-8">
-          <button className="btn btn-xs bg-blue-400 rounded-2xl">
-            LEARN MORE
-          </button>
-        </div>
-      </div>
+        <p>Our Vision:</p>
+        <p className="mb-4">
+          We are committed to being the Quality leader in Production, Process,
+          and Distribution of safe products to the consumer.
+        </p>
+        <p>Our Mission:</p>
+        <p className="mb-4">
+          We are committed to contributing to the sustainable growth of the
+          overall economy of the country by conducting all our business
+          operations through.
+        </p>
+        <p>
+          Innovation and introduction of proper products & services that our end
+          users deserve.
+        </p>
+        <p>
+          Development of skilled human resources as per the need of the
+          respective industry.
+        </p>
+        <p>
+          Establishment of a healthy trade relationship with a dynamic &
+          challenging global market.
+        </p>
+        <p className="mt-4">
+          We are further committed to train and motivate all our employees in
+          order to provide superior quality service to every person we interact.
+        </p>
+        <p className="mt-4">
+          Animal Agriculture (Dairy, Poultry & Aquaculture Integration)
+        </p>
+        <p>Animal Feed</p>
+        <p>Agro-chemical</p>
+        <p>Agro Machineries</p>
+        <p>
+          Crop Agriculture (Seed, Fertilizer, Pesticides, Insecticides etc.)
+        </p>
+        <p>Consumer Food</p>
+        <p>Pharma & Healthcare</p>
+        <p className="mt-4">Franchise Restaurant (bb.q)</p>
+        <p>Media</p>
+        <p className="mt-4">
+          Eon Bazar is an online market place of Eon Group committed to deliver
+          wide range of consumer food products to its consumers. All food
+          products are antibiotic & chemical free, freshly produced which are
+          sourced through network farming and maintaining proper cold chain.
+        </p>
+        <p className="mt-4">
+          The production facility of different brands under this group of
+          companies are Halal, ISO and HACCP certified to ensure food safety and
+          security to consumers.
+        </p>
 
-      {/*========= picture section ========= */}
-      <div className="w-full">
-        <img src="/src/assets/image/Ecommerce web page (1).gif" alt="" />
+        <p className="mt-4">
+          Eon bazar is currently offering different reliable brands like Country
+          Natural, Baqarah, Ocean Blue, Ocean Ria and Puro.
+        </p>
       </div>
     </section>
   );
